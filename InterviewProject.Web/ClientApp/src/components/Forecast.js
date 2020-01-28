f01ef83d1a833f83589db27afd3cc00affc5fbaa
Dmytro Sviridov <dmytro.sviridov@autopoint.com>
2020-01-28 16:53:03 -0700

    Format date

--- InterviewProject.Web/ClientApp/src/components/Forecast.js
+++ InterviewProject.Web/ClientApp/src/components/Forecast.js
@@ -2,11 +2,22 @@ import React from 'react'
 
 import './Forecast.css'
 
+var moment = require('moment')
+
+moment.updateLocale('en', {
+    calendar: {
+        sameDay: '[Today]',
+        nextDay: '[Tomorrow]',
+        nextWeek: 'dddd',
+        sameElse: 'L'
+    }
+})
+
 export const Forecast = (props) => {
     const { date, temperature, summary } = props
 
     return (<div className="forecast">
-        <div className="date">{date}</div>
+        <div className="date">{moment(date, 'YYYY-MM-DD').calendar()}</div>
         <div className="temperature">{Math.round(temperature)} °C</div>
         <div className="summary">{summary}</div>
     </div>)
--- InterviewProject.Web/ClientApp/src/components/Forecast.test.js
+++ InterviewProject.Web/ClientApp/src/components/Forecast.test.js
@@ -3,10 +3,12 @@ import { shallow } from 'enzyme'
 
 import { Forecast } from './Forecast'
 
+var moment = require('moment')
+
 describe('Forecast component', () => {
     it('should render', () => {
         const fixture = shallow(<Forecast date="2020-01-01" temperature={12.34} summary="Snow" />)
-        expect(fixture.find('div.date').text()).toBe('2020-01-01')
+        expect(fixture.find('div.date').text()).toBe('01/01/2020')
         expect(fixture.find('div.temperature').text()).toBe('12 °C')
         expect(fixture.find('div.summary').text()).toBe('Snow')
     })
@@ -16,5 +18,12 @@ describe('Forecast component', () => {
         expect(fixture.find('div.temperature').text()).toBe('13 °C')
         fixture.setProps({ temperature: 15.5 })
         expect(fixture.find('div.temperature').text()).toBe('16 °C')
+    })
+
+    it('should format date', () => {
+        const fixture = shallow(<Forecast date={moment().format('YYYY-MM-DD')} temperature={12.64} summary="Snow" />)
+        expect(fixture.find('div.date').text()).toBe('Today')
+        fixture.setProps({ date: moment().add(1, 'day').format('YYYY-MM-DD') })
+        expect(fixture.find('div.date').text()).toBe('Tomorrow')
     })
 })
